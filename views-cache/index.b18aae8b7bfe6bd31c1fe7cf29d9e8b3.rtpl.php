c22dc01a6a9ff59d71dda3ade4f8a242b23cfa69
Kleyton João <35746510+kleyton032@users.noreply.github.com>
2018-02-27 00:52:52 -0300

    template admin e admin tpl

---
+++ views-cache/index.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -0,0 +1,17 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?>
+              <!-- Menu Body -->
+              <li class="user-body">
+                <div class="row">
+                  <div class="col-xs-4 text-center">
+                    <a href="#">Followers</a>
+                  </div>
+                  <div class="col-xs-4 text-center">
+                    <a href="#">Sales</a>
+                  </div>
+                  <div class="col-xs-4 text-center">
+                    <a href="#">Friends</a>
+                  </div>
+                </div>
+                <!-- /.row -->
+              </li>
+             
