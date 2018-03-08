921a9333f33a601c68f40e96175e823f7d665092
Kleyton João <35746510+kleyton032@users.noreply.github.com>
2018-03-07 22:29:06 -0300

    crud usuário admin

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -81,6 +81,7 @@ class User extends Model{
 	}
 
 	public function get($iduser){
+		
 		$sql = new Sql();
 
 		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
@@ -88,7 +89,7 @@ class User extends Model{
 
 		));
 
-		$this->setData($results);
+		$this->setData($results[0]);
 
 
 	}
@@ -109,6 +110,17 @@ class User extends Model{
 		$this->setData($results[0]);
 
 
+
+	}
+
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+
+			":iduser"=>$this->getiduser()
+		));
 
 	}
 
--- views-cache/users.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
+++ views-cache/users.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -26,7 +26,6 @@
               <table class="table table-striped">
                 <thead>
                   <tr>
-                    <th style="width: 10px">#</th>
                     <th>Nome</th>
                     <th>E-mail</th>
                     <th>Login</th>
@@ -37,7 +36,6 @@
                 <tbody>
                   <?php $counter1=-1;  if( isset($users) && ( is_array($users) || $users instanceof Traversable ) && sizeof($users) ) foreach( $users as $key1 => $value1 ){ $counter1++; ?>
                   <tr>
-                    <td><?php echo htmlspecialchars( $value1["iduser"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>
                     <td><?php echo htmlspecialchars( $value1["desperson"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>
                     <td><?php echo htmlspecialchars( $value1["desemail"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>
                     <td><?php echo htmlspecialchars( $value1["deslogin"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td/>
